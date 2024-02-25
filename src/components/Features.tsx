ec56b6d7c830174c3e0aa3ff0825be27bb75d778
Delziron Lima <delziron.braz@hotmail.com>
2024-02-25 13:51:08 -0300

    style: fix card grid

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -11,21 +11,19 @@ const Features = () => {
     if (data) {
         console.log(data)
         return (
-            <section id="features" className="min-h-screen flex flex-col justify-center pt-24">
+            <section id="features" className="min-h-screen flex flex-col items-center justify-center pt-24">
                 <h1 className="font-semibold text-4xl text-center text-ExtraDarkColor">Exclusive Products</h1>
 
-                <div className="flex flex-wrap justify-center gap-5 pt-8">
-                    <div className="md:w-2/4 lg:w-1/4">
-                        {data.map((product: Product) => (
-                            <ProductCard
-                                key={product.id}
-                                id={product.id}
-                                title={product.title}
-                                image={product.image}
-                                price={product.price}
-                            />
-                        ))}
-                    </div>
+                <div className="flex flex-wrap justify-center items-center gap-5 pt-8 w-3/4">
+                    {data.map((product: Product) => (
+                        <ProductCard
+                            key={product.id}
+                            id={product.id}
+                            title={product.title}
+                            image={product.image}
+                            price={product.price}
+                        />
+                    ))}
                 </div>
             </section>
         )
